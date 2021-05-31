9559910c5149b3ebe1f08397df2cb6868bf37268
coder samuel <samuelsantos.ss95@gmail.com>
2021-05-31 13:38:22 -0300

    Usuário, Postagem e Tema

---
+++ src/app/model/User.ts
@@ -0,0 +1,15 @@
+import { Postagem } from "./Postagem"
+import { Tema } from "./Tema"
+
+export class User {
+public id: number
+public nome: string
+public usuario: string
+public senha: string
+public foto: string
+public token : string
+public tipo: string
+public postagem : Postagem []
+public tema : Tema
+
+}
